3b00dddb801e2f2da0c4f7da33c83e5fe90ece5a
grandballoon <luke.sovern.conley@gmail.com>
2018-07-03 18:01:11 -0400

    pokemon

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+
+attr_accessor :name, :type, :id, :db, :hp
+
+def initialize(id:, name:, type:, db:)
+  @name = name
+  @type = type
+  @id = id
+  @db = db
+end
+
+def self.save(name, type, db)
+  sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+
+  db.execute(sql, name, type)
+end
+
+def self.find(id, db)
+  sql = "SELECT * FROM pokemon WHERE id = ?"
+  pokemon_array = db.execute(sql, id)
+  poke = pokemon_array.flatten
+  Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db: db)
+end
+
+
+def alter_hp(int, db)
+  sql = "UPDATE pokemon SET hp = ? WHERE name = ?"
+  self.hp = int
+  db.execute(sql, int, self.name)
+end
+
+
 end
